9601cb5463b7156163dbf3c606602e180ed744fc
Brooke Ross <brooke.ross2800@gmail.com>
2022-09-30 15:49:53 -0600

    added files

---
+++ project2/src/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Words for our app here--APP.js</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
