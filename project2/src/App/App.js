6bd482e400640b581727ee0fec983185277c617a
Brooke Ross <brooke.ross2800@gmail.com>
2022-10-03 12:00:43 -0600

    added NavBar things

--- project2/src/App/App.js
+++ project2/src/App/App.js
@@ -1,19 +1,43 @@
 import './App.css';
+import React, {useState} from "react";
+import {Switch, Route} from "react-router-dom"
 import NavBar from "../NavBarFolder/NavBar.js"
+import About from "../NavBarFolder/About.js"
+import Favorites from "../NavBarFolder/Favorites"
+import Home from "../NavBarFolder/Home"
 import Header from "../HeaderFolder/Header"
 import Search from "../SearchFolder/Search"
 import CardContainer from "../CardContainerFolder/CardContainer"
 
 function App() {
+  const [page, setPage]=useState("/")
+
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar />
         <Header />
         <Search />
         <p>Words for our app here--APP.js</p>
         <CardContainer />
       </header>
+
+      <div>
+        <NavBar onChangePage={setPage}/>
+        <Switch>
+          <Route path = "/about">
+            <About />
+          </Route>
+          <Route path = "/favorites">
+            <Favorites />
+          </Route>
+          <Route eact path = "/">
+            <Home />
+          </Route>
+          <Route path ="*">
+            <h1>404 not found</h1>
+          </Route>
+        </Switch>
+      </div>
        
     </div>
   );
