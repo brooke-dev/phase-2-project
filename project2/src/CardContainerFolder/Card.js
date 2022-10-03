13bfd948152a2d553c387ba089fa3c27a5abfe79
Chris Williams <cjwolliams@gmail.com>
2022-10-03 16:07:45 -0700

    nothing

--- project2/src/CardContainerFolder/Card.js
+++ project2/src/CardContainerFolder/Card.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import CardDetails from "./CardDetails"
 
-function Card(){
+function Card({game}){
+    const {name, image} = game
+    let [showDetails,setShowDetails] = useState(true)
+
+    function handleDetails() {
+        setShowDetails(showDetails = showDetails);
+    }
     return (
-        <div>
+        <div className="Card" onClick={handleDetails}>
            <p>This is the card.js component</p> 
            <CardDetails />
         </div>
