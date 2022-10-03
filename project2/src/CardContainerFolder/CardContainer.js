1f5115f0e068216536265e09c514998b70786590
chrisdennis12345@gmail.com <chrisdennis@Chriss-iMac.local>
2022-10-03 14:24:12 -0700

    fetch

--- project2/src/CardContainerFolder/CardContainer.js
+++ project2/src/CardContainerFolder/CardContainer.js
@@ -1,12 +1,54 @@
-import React from "react";
-import Filter from "./Filter"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Filter from "./Filter";
+import cards from "../data/db.json" ;
+import Card from "./Card.js"
 
 function CardContainer(){
+
+
+
+    const [pokemon, setpokemon] = useState([])
+    const [magic, setmagic] = useState([])
+    const [yugioh, setyugioh] = useState([])
+
+
+    const fetchData = () => {
+        const pokemonUrl = "http://localhost:4000/Pokemon"
+        const magicUrl =  "http://localhost:4000/Magic-the-Gathering"
+        const yugiohUrl = "http://localhost:4000/Yu-Gi-Oh!"
+        
+        const getPokemon = axios.get(pokemonUrl)
+        const getMagic = axios.get(magicUrl)
+        const getYugioh = axios.get(yugiohUrl)
+        
+        axios.all([getPokemon, getMagic, getYugioh]).then(
+            axios.spread((...allData) => {
+                const allPokemonData = allData[0]
+                const allMagicData = allData[1]
+                const allYugiohData = allData[2]
+
+                console.log(allPokemonData)
+                 console.log(allYugiohData)
+                 console.log(allMagicData)
+            })
+        )
+    }
+       
+useEffect(() => {
+    fetchData()
+}, []);
+
+
+
+
     return (
-        <div>
+        <ul className="cards">
+            
             <Filter />
+            
             <p> this is the CardContainer.js component</p>
-        </div>
+        </ul>
     )
 }
 
